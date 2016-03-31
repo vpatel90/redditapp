27cf108c6b787fc086eba9b4f1d606609258210e
Vivek Patel <vnp229@gmail.com>
2016-03-31 14:55:03 -0500

    seed file generated

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,9 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :link
+
+  validates :user_id, uniqueness: {
+    scope: :link_id,
+    message: "cannot vote for the same item more than once"
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+25.times do
+  user = User.create(user_name:Faker::Internet.user_name, email:Faker::Internet.email)
+  rand(0..5).times do
+    Link.create(title:Faker::Hipster.sentence(4,false,7), url:'#', user_id:user.id)
+  end
+end
+
+value = [1,1,-1]
+1000.times do
+  Vote.create(user_id:rand(1..User.count), link_id:rand(1..Link.count), value:value.sample)
+end
